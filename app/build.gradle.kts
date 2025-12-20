67ad3f092109bd9351bd1bc33e5da6a1253adcd4
mukesh22584 <mks22584@gmail.com>
2025-12-20 17:59:29 +0530

    build: add namespace

---
+++ app/build.gradle.kts
@@ -0,0 +1,12 @@
+android {
+    compileSdk = 34
+    namespace = "com.example.gerin.inventory"
+
+    defaultConfig {
+        applicationId = "com.example.gerin.inventory"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
